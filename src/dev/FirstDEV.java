d2c853e9a0f40d8b6014bf858f19c3721480a15d
ant <xiedehui1995@163.com>
2019-07-14 11:10:54 +0800

    commit request

---
+++ src/dev/FirstDEV.java
@@ -0,0 +1,14 @@
+package dev;
+
+/**
+ * @author Ant
+ * @date 2019-07-14 11:09
+ */
+public class FirstDEV {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "hello the first DEV"
+        );
+    }
+}
